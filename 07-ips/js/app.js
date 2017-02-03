c5753bead22b9aed85c6fba452caafbb72bbf695
vertebrand <alvarosg88@gmail.com>
2017-02-03 18:57:38 +0100

    api externa

--- 07-ips/js/app.js
+++ 07-ips/js/app.js
@@ -2,17 +2,17 @@
 
 var app = angular.module('ejemplosApp',[ ]);
 
-
-
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
-  
-  //http://www.geoplugin.net/json.gp?jsoncallback=JSON_CALLBACK
+
   $scope.geo = {};
-  $http.jsonp('http://www.geoplugin.net/json.gp?jsoncallback=JSON_CALLBACK').success(function(data){
+  $http.get('http://api.openweathermap.org/data/2.5/weather?q=Cordoba,es&appid=????????').success(function(data){
       $scope.geo = data;
       console.log(data)
   })
 
+  $scope.toCls = function(k){
+      return Math.round( (k - 273,15) * 100 ) / 100;
+  }
 }]);
 
 // Creo filtro propio para mostrar correctamente valores con caracteres Unicode
