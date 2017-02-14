26a473d7da9d7c8b749fea568d11cfb4d63e3cfe
vertebrand <alvarosg88@gmail.com>
2017-02-14 19:38:20 +0100

    post

--- 09-UniverApp/js/alumnoDetalleCtrl.js
+++ 09-UniverApp/js/alumnoDetalleCtrl.js
@@ -4,15 +4,75 @@ app.controller('alumnoDetalleCtrl', ['$scope','$http', '$routeParams',function($
   
     var codigo = $routeParams.codigo;
     $scope.alumno = {};
-    $http.get('php/servicios/alumnos.getAlumno.php?c=' + codigo ).success(function(data){
-    console.log(data)
-      if ( data.err ){
-          window.location.replace('#/alumnos')
-          return;
-      }else{
-          $scope.alumno = data;  
-      }   
-    });
+    $scope.showSuccess = false;
+    $scope.showDanger = false;
+    $scope.nuevoAlumno = false;
+
+    if ( codigo != 'nuevo' ){
+
+        $http.get('php/servicios/alumnos.getAlumno.php?c=' + codigo ).success(function(data){
+            console.log(data)
+              if ( data.err ){
+                  window.location.replace('#/alumnos')
+                  return;
+              }else{
+                  $scope.alumno = data;
+              }
+        });
+
+    }else{
+
+        $scope.nuevoAlumno = true;
+
+    }
+
+
+
+    $scope.guardarAlumno = function(){
+
+        if ( !$scope.nuevoAlumno ){
+
+            $http.post('php/servicios/alumnos.guardar.php',$scope.alumno).success(function(data){
+
+                updateStudent(data);
+
+            });
+
+        }else{
+
+            $http.post('php/servicios/alumnos.crear.php',$scope.alumno).success(function(data){
+
+                updateStudent(data);
+
+            });
+
+        }
+
+
+
+    }
+
+    function updateStudent(data){
+        if( data.err ){
+
+                $scope.showDanger = true;
+                setTimeout(function(){
+                    $scope.showDanger = false;
+                    $scope.$apply();
+                },4000)
+                console.log(data)
+
+            }else{
+
+                $scope.showSuccess = true;
+                setTimeout(function(){
+                    $scope.showSuccess = false;
+                    $scope.$apply();
+                },4000)
+                console.log(data)
+
+            }
+    }
 
 }]);
 
